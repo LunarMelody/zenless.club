45f0ae71100745a98dd7014dd6a05c3d6e72c64f
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-18 00:23:27 +0400

    home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { RouterLink } from "vue-router";
+import Main from "@/components/Main.vue";
 </script>
 
 <template>
-  <main class="container mx-auto max-w-3xl">
-    <h1>helo</h1>
-    <RouterLink to="/about">read about</RouterLink>
-  </main>
+  <Main :vertical-center="true">
+    <h1 class="text-2xl md:text-4xl font-bold text-gray-300">
+      <span class="animate-pulse">Waiting for content...</span>
+    </h1>
+  </Main>
 </template>
