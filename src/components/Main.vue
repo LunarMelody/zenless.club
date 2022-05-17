426d11d98b454424cf922376a37177dc3ffa7487
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-18 00:22:52 +0400

    main wrapper

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+
+<script setup lang="ts">
+interface Props {
+  verticalCenter?: boolean;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <main class="main p-4 lg:p-8 grid" :class="verticalCenter ? 'place-content-center' : ''">
+    <div id="content">
+      <slot></slot>
+    </div>
+  </main>
+</template>
