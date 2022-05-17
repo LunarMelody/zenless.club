905f997d08548f61bbd51c867ebf8cef8b4b6255
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-18 00:23:09 +0400

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+
+<template>
+  <footer
+    class="footer grid place-content-center py-4 mx-4 md:mx-32 lg:mx-64 border-t border-gray-200 dark:border-gray-200/5"
+  >
+    <span
+      class="transition-all duration-300 ease-in-out text-gray-500 hover:text-gray-700 dark:hover:text-gray-200 hover:opacity-100"
+    >
+      <a href="https://github.com/kitsune-guuji/zenless.club" target="_blank">
+        <span>&nbsp;source code</span>
+      </a>
+    </span>
+  </footer>
+</template>
