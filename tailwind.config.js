1338574ae1f1aea76b0c247e7a9716733e4967a4
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-17 23:35:56 +0400

    fix annoying eslint, add primary+secondary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+/* eslint-env node */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.vue"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.pink,
+        secondary: colors.sky,
+      },
+    },
   },
   plugins: [],
 };
