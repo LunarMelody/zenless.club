c7496c674601b317c233c6897bd6c1e2fb436d0a
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-05-18 13:44:18 +0400

    use webhashistory instead

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
